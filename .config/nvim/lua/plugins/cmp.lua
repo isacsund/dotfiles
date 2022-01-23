ecf8b8688a3ba0bde37bf1249dfda7f70e8fbb19
Isac Sund <isac.sund@outlook.com>
2022-01-23 14:49:54 +0200

    nvim: remove lspkind
    No need to use lspkind when the functionality can be implemented with a
    single function.

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -4,6 +4,34 @@ lspkind.init()
 
 local M = {}
 
+vim.lsp.protocol.CompletionItemKind = {
+  Text = "[text]",
+  Method = "[method]",
+  Function = "[function]",
+  Constructor = "[constructor]",
+  Field = "[field]",
+  Variable = "[variable]",
+  Class = "[class]",
+  Interface = "[interface]",
+  Module = "[module]",
+  Property = "[property]",
+  Unit = "[unit]",
+  Value = "[value]",
+  Enum = "[enum]",
+  Keyword = "[key]",
+  Snippet = "[snippet]",
+  Color = "[color]",
+  File = "[file]",
+  Reference = "[reference]",
+  Folder = "[folder]",
+  EnumMember = "[enum member]",
+  Constant = "[constant]",
+  Struct = "[struct]",
+  Event = "[event]",
+  Operator = "[operator]",
+  TypeParameter = "[type]",
+}
+
 -- Better commpletion
 -- menuone: popup even when there's only one match
 -- noinsert: do not insert text until a selection is made
@@ -48,15 +76,18 @@ cmp.setup({
     { name = "buffer", priority = 10, keyword_length = 5 },
   },
   formatting = {
-    format = lspkind.cmp_format({
-      with_text = true,
-      menu = {
-        buffer = "[buf]",
+    format = function(entry, vim_item)
+      local menu_map = {
+        buffer = "[Buf]",
         nvim_lsp = "[LSP]",
-        path = "[path]",
-        vsnip = "[snip]",
-      },
-    }),
+        path = "[Path]",
+        vsnip = "[Snip]",
+      }
+      vim_item.menu = menu_map[entry.source.name] or string.format("[%s]", entry.source.name)
+
+      vim_item.kind = vim.lsp.protocol.CompletionItemKind[vim_item.kind]
+      return vim_item
+    end,
   },
   experimental = {
     ghost_text = true,
